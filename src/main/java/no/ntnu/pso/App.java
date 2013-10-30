e64bef12c9a432c2f8a9e6129c5cce860ff6110d
jantorestolsvik <jan.tore@stoelsvik.no>
2013-10-30 17:38:29 +0100

    Added some basic functionality

--- src/main/java/no/ntnu/pso/App.java
+++ src/main/java/no/ntnu/pso/App.java
@@ -1,13 +1,54 @@
 package no.ntnu.pso;
 
+import com.sun.j3d.internal.Distance;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Hello world!
  *
+ * @author Jan Tore Stølsvik & Tom Glover
  */
 public class App 
 {
+    public static double distance(double[] x, double[] y) {
+        double sum = 0.0;
+        for (int i = 0; i < x.length; i++) {
+            sum += (x[i] - y[i]) * (x[i] - y[i]);
+        }
+        return sum;
+    }
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Start" );
+        List<Boid> boids = new ArrayList();
+        
+        //DIMENSION SPESIFFIC
+        for (int i = 0; i < 5; i++) {
+            boids.add(new Boid(1));
+        }
+        double[] goal = new double[1];
+        double[] bestGlobalPosition = new double[1];
+        //DIMENSION SPESIFFIC
+        
+        double bestGlobalDistance = Double.MAX_VALUE;
+        
+        for (int i = 0; i < 200; i++) {
+            for (Boid boid : boids) {
+                double boidDistance = distance(boid.getPosition(), goal);
+                if (boidDistance < bestGlobalDistance ) {
+                    bestGlobalDistance = boidDistance;
+                    bestGlobalPosition = boid.getPosition().clone();
+                }
+            }
+            for (Boid boid : boids) {
+                boid.nextIteration(bestGlobalPosition);
+            }
+            System.out.println(bestGlobalPosition[0]);
+        }
+        
+        for (Boid boid : boids) {
+            System.out.println(boid);
+        }
     }
 }
---
+++ src/main/java/no/ntnu/pso/Boid.java
@@ -0,0 +1,63 @@
+package no.ntnu.pso;
+
+/**
+ *
+ * @author Jan Tore Stølsvik & Tom Glover
+ */
+public class Boid {
+    private int dimensions;
+    private double[] velocity;
+    private double[] position;
+    private double[] bestSeenPosition;
+    private double c1,c2,c3,r1,r2,minValue = -10.0,maxValue = 10.0;
+    
+    public Boid(int dimensions) {
+        this.dimensions = dimensions;
+        this.velocity = new double[dimensions];
+        this.position = new double[dimensions];
+        this.bestSeenPosition = new double[dimensions];
+        for (int i=0;i<dimensions;i++) {
+            this.position[i] = Math.random() * 10;
+            this.bestSeenPosition[i] = this.position[i];
+        }
+        c1 = 0.2;
+        c2 = 2.0;
+        c3 = 0.2;
+    }
+
+    private double clamp(double value) {
+        return Math.max(minValue, Math.min(value, maxValue));
+    }
+    
+    public void nextIteration(double[] bestGlobalPosition) {
+        for (int i = 0; i < dimensions; i++) {
+            //Not sure if r1 and r2 should be random every iteration
+            //New velocity
+            r1 = Math.random();
+            r2 = Math.random();
+            velocity[i] = (c3 * velocity[i]) + (c1 * r1 * (bestSeenPosition[i] - position[i]) + (c2 * r2 * (bestGlobalPosition[i] - position[i])));
+            velocity[i] = clamp(velocity[i]);
+            
+            //New position
+            position[i] = position[i] + velocity[i];
+        }
+    }
+
+    public double[] getPosition() {
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("( ");
+        for (int i=0;i<dimensions;i++) {
+            sb.append(position[i]);
+            if (i!=dimensions-1) {
+                sb.append(" , ");
+            }
+            sb.append(" )");
+        }
+        return sb.toString();
+    }
+}
